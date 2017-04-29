163ddb79352e09d3d85023bb87977bad0f78de10
kmaitski <kevin.maitski@gmail.com>
2017-04-29 12:56:22 -0700

    close need to fix some complier errors then I can test

--- get_next_line.c
+++ get_next_line.c
@@ -1,15 +1,56 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line2.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: kmaitski <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/28 10:37:16 by kmaitski          #+#    #+#             */
-/*   Updated: 2017/04/28 10:44:14 by kmaitski         ###   ########.fr       */
+/*   Updated: 2017/04/29 12:53:47 by kmaitski         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+/******************************************************
+**               funtion prototypes                 ***
+*******************************************************/
+unsigned int check_for_new_line(char *str);
+void intialize_line(char *tmp, char *line);
+int read_into_buffer(int fd, char *tmp);
+int get_next_line(int fd, char **line);
+
+/* 
+ * ===  FUNCTION  ==============================================================
+ *         Name:  check_for_new_line
+ *  Description:  Check for a \n character and return the location.  Returns -1
+ *  			  if not found.
+ * =============================================================================
+ */
+unsigned int	check_for_new_line(char *str)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (str[i] != '\n')
+		i++;
+	return (i + 1);
+}		/* -----  end of function check_for_new_line  ----- */
+
+
+/* 
+ * ===  FUNCTION  ==============================================================
+ *         Name:  intialize_line
+ *  Description:  Intialize line with tmp.
+ * =============================================================================
+ */
+void			intialize_line(char *tmp, char *line)
+{
+	unsigned int	i;
+
+	i = check_for_new_line(tmp);
+	line = ft_strsub(tmp, 0, i);
+	tmp = ft_strdup(tmp, 0, ++i);
+}		/* -----  end of function intialize_line  ----- */
 
 /* 
  * ===  FUNCTION  ==============================================================
@@ -17,13 +58,23 @@
  *  Description:  Read from the file descriptor into the buffer.
  * =============================================================================
  */
-void	read_into_buffer (int fd, char **line)
+int			read_into_buffer(int fd, char *tmp)
 {
-	int	read_bytes;
+	int		read_bytes;
+	char	buffer[BUFF_SIZE];
 
-	while ((read_bytes = read(
-	return <+return_value+>;
+	while ((read_bytes = read(fd, buffer, BUFF_SIZE)) > 0)
+	{
+		if (!tmp)
+			tmp = ft_strdup(buffer);
+		else
+			tmp = ft_strjoin(tmp, buffer);
+	}
+	if (read_bytes < 0)
+		tmp = ft_strnew(1);
+	return (read_bytes);
 }		/* -----  end of function read_into_buffer  ----- */
+
 /* 
  * ===  FUNCTION  ==============================================================
  *         Name:  get_next_line
@@ -31,10 +82,22 @@ void	read_into_buffer (int fd, char **line)
  *  			  descriptor (fd)
  * =============================================================================
  */
-int		get_next_line (int fd, char **line)
+int				get_next_line(int fd, char **line)
 {
+	char	*tmp;
+	int		read_bytes;
+
 	if (fd < 0 || !line)
 		return (-1);
-	read_into_buffer(fd, line);
+	if (!tmp)
+	{
+		tmp = ft_strnew(1);
+		read_bytes = read_into_buffer(fd, &tmp);
+	}
+	if (tmp)
+	{
+		intialize_line(&tmp, &line);
+		return (1);
+	}
 	return (0);
 }		/* -----  end of function get_next_line  ----- */
