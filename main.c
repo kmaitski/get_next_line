f78529e0f32218da424f3022b6ade340cb1c40b3
kmaitski <kevin.maitski@gmail.com>
2017-05-16 16:29:23 -0700

    passed all easy test but I am getting a sig abrt with the super long string

--- main.c
+++ main.c
@@ -25,16 +25,34 @@ int	main(void)
 {
 	int	fd = open("textfile.txt", O_WRONLY);
 	char *line;
-	int	nbr;
+//	int	error_return;
+//	char	*str;
+//	int	len = 50;
 
-	write(fd, "abc\n\n", 5);
+//	str = (char *)malloc(1000 * 1000);
+//	*str = '\0';
+//	while (len--)
+//		ft_strcat(str, "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur in leo dignissim, gravida leo id, imperdiet urna. Aliquam magna nunc, maximus quis eleifend et, scelerisque non dolor. Suspendisse augue augue, tempus");
+//	printf("%d", (int)ft_strlen(str));
+//	write(fd, str, ft_strlen(str));
+
+
+	write(fd, "mnopqrstuvwxyzab", 16);
+//	write(fd, "efgh\n", 5);
+//	write(fd, "ijkl\n", 5);
+//	write(fd, "mnop\n", 5);
+//	write(fd, "qrst\n", 5);
+//	write(fd, "uvwx\n", 5);
+//	write(fd, "yzab\n", 5);
 	close(fd);
 	fd = open("textfile.txt", O_RDONLY);
-	nbr = get_next_line(42, &line);
-	printf("%d", nbr);
-//	while (get_next_line(-99, NULL) > 0)
-//	{
-//		printf("%s", line);
-//		printf("%d", (int)ft_strlen(line));
-//	}
+/*  			error tests 				*/
+//	error_return = get_next_line(42, &line);
+//	printf("%d", error_return);
+	while (get_next_line(fd, &line) > 0)
+	{
+		printf("%s", line);
+		printf("%d", (int)ft_strlen(line));
+//		printf("%d", ft_strcmp(str, line));
+	}
 }
